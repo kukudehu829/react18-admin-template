71ea5f018f37272fed9eaad790943a3c9716c92b
dianhu.liu <liudianhu829@163.com>
2024-10-12 14:13:29 +0800

    feat: 项目初始化

---
+++ README.md
@@ -0,0 +1,16 @@
+个人前端内容汇总
+node: 18.20.0 pnpm
+
+主应用: {
+  React,
+  create-react-app,
+  Antd,
+  qiankun.js
+}
+子应用: {
+  Vue3,
+  vite,
+  element,
+  qiankun.js,
+  keep-alive
+}
---
+++ build/webpack.base.js
@@ -0,0 +1,69 @@
+// webpack.base.js
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: path.join(__dirname, '../src/index.tsx'), // 入口文件
+   // 打包文件出口
+  output: {
+    filename: 'static/js/[name].js', // 每个输出js的名称
+    path: path.join(__dirname, '../dist'), // 打包结果输出路径
+    clean: true, // webpack4需要配置clean-webpack-plugin来删除dist文件,webpack5内置了
+    publicPath: '/' // 打包后文件的公共前缀路径
+  },
+  module: {
+    rules: [
+      {
+        test: /.(ts|tsx)$/, // 匹配.ts, tsx文件
+        use: {
+          loader: 'babel-loader',
+          options: {
+            // 预设执行顺序由右往左,所以先处理ts,再处理jsx
+            presets: [
+              '@babel/preset-react',
+              '@babel/preset-typescript'
+            ]
+          }
+        }
+      },
+      {
+        test: /.(ts|tsx)$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            // 执行顺序由右往左,所以先处理ts,再处理jsx,最后再试一下babel转换为低版本语法
+            presets: [
+              [
+                "@babel/preset-env",
+                {
+                  // 设置兼容目标浏览器版本,这里可以不写,babel-loader会自动寻找上面配置好的文件.browserslistrc
+                  // "targets": {
+                  //  "chrome": 35,
+                  //  "ie": 9
+                  // },
+                   "useBuiltIns": "usage", // 根据配置的浏览器兼容,以及代码中使用到的api进行引入polyfill按需添加
+                   "corejs": 3, // 配置使用core-js低版本
+                  }
+                ],
+              '@babel/preset-react',
+              '@babel/preset-typescript'
+            ]
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.tsx', '.ts'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '../public/index.html'), // 模板取定义root节点的模板
+      inject: true, // 自动注入静态资源
+    }),
+    new webpack.DefinePlugin({
+      'process.env.BASE_ENV': JSON.stringify(process.env.BASE_ENV)
+    })
+  ]
+}
